025683695fc83e9f13526617fd42fd7c48f9249f
Tabish <mohdtabishkhan001@gmail.com>
2024-03-04 22:48:39 +0530

    added orange icons

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import github from "./assets/icons/github.png"
 import peerlist from "./assets/icons/peerlist.png"
 import twitter from "./assets/icons/twitter.png"
 import linkedIn from "./assets/icons/linkedin.png"
-
 import email from "./assets/icons/email.png"
+
+import peerlist_orange from "./assets/icons/peerlist_orange.png"
+import twitter_orange from "./assets/icons/twitter_orange.png"
+import linkedIn_orange from "./assets/icons/linkedIn_orange.png"
+
 
 
 
  
-export {github,peerlist,linkedIn,twitter,email}
+export {github,peerlist,linkedIn,twitter,email,linkedIn_orange,twitter_orange,peerlist_orange}
